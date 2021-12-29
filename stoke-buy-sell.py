309de5dcda496bc7953949a3511e0e0a78198769
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2021-12-30 01:13:21 +0600

    Max profit with Two pointer

---
+++ stoke-buy-sell.py
@@ -0,0 +1,14 @@
+def maxProfit( prices):
+    l, r = 0 ,1
+
+    maxP = 0
+
+    while r < len(prices):
+        if prices[r] > prices [l]:
+            profit = prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        else:
+            l = r
+        r = r+1
+    return maxP
+
