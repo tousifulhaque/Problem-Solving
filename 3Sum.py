edc0a74b2586685988d6fb04870c436fce8648c3
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-01-07 00:08:56 +0600

    O(n^2) for 3Sum

--- 3Sum.py
+++ 3Sum.py
@@ -1,16 +1,19 @@
 def threeSum(nums):
+
     value_map = {}
-    result = set()
-    for index , value in enumerate(nums):
+    result = []
+    for index, value in enumerate(nums):
         value_map[value] = index
-    if len(nums) >= 2 :
+    if len(nums) <= 2 :
         return result
 
     for i in range(len(nums)):
-        for j in range(len(nums)):
-            if j != i:
+        for j in range(i+1, len(nums)):
+            if j != i :
+                value = -(i+j)
+                if value in value_map and value_map[value] != i and value_map[value] != j:
+                    result.append([nums[i], nums[j], value])
+    return result
 
-    return value_map
-
-print(threeSum([1,2]))
+print(threeSum([-1,0,1,2,-1,-4]))
 
