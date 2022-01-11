bdf4e224f931682137f478b4f146dd3f78306fcc
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-01-11 23:56:53 +0600

    WIP : Longest Repeating String

--- longestRepeatingChar.py
+++ longestRepeatingChar.py
@@ -1,3 +1,15 @@
 def characterReplacement(s, k):
 
-    s.replace(s[])
+    char_map = {}
+    l = 0
+    num_a = 0
+
+    for r in range(len(s)):
+
+        if s[r] in char_map:
+            char_map[s[r]] += 1
+
+        else:
+            char_map[s[r]] = 1
+
+        while l - r - k +1 not in char_map.values():
