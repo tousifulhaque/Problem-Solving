95a8a9aee0b3cb38401a18bd442b8733d6da69b4
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-01-14 03:29:07 +0600

    MinSubString 2nd Attempt

--- mininumSubString.py
+++ mininumSubString.py
@@ -3,7 +3,9 @@ def minimumSubString(s, t):
     smap = {}
     l = 0
     r = 0
-    min_string = s
+    min_len = float('inf')
+    min_string = ''
+    position_track = []
     for i in t:
         if i in tmap:
             tmap[i] += 1
@@ -11,21 +13,26 @@ def minimumSubString(s, t):
             tmap[i] = 1
 
     while r < len(s):
-        if s[r] in tmap: #A
-            if s[r] in smap:
-                smap[s[r]] += 1 #{A: 1, B:1, C:1  }
+        if s[r] in tmap:  # B
+            if s[r] in smap :
+                smap[s[r]] += 1
+                if smap[s[r]] > tmap[s[r]] :
+                    l = r
+                    smap = {s[r]: 1}
             else:
                 smap[s[r]] = 1
-
-            while smap == tmap :
-                if r-l+1 < len(min_string) :
-                    min_string = s[l:r+1]
+            position_track.append(r)  # [0, 3, 5]
+            if smap == tmap:
+                if r - l + 1 < min_len:
+                    min_len = r - l + 1  # 5
+                    min_string = s[l:r + 1]
                 smap[s[l]] -= 1
-                l -= 1
+                l = position_track.pop(0)  # 3
         r += 1
     return min_string
 
-s = "ADOBECODEBANC"
-t = "ABC"
 
-print(minimumSubString(s,t))
+s = "ab"
+t = "b"
+
+print(minimumSubString(s, t))
