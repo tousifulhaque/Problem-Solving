142db6e9d8aa809ba38a387b41570c3e4cb30c29
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-01-23 03:03:28 +0600

    Valid Parantheses 2nd

--- validParantheses.py
+++ validParantheses.py
@@ -1,16 +1,36 @@
-def isValid(self, s: str) -> bool:
-        paran_dic = {
+def isValid(s):
+    paran_dic = {
             '(' : ')',
             '{' : '}',
             '[' : ']'
 
         }
+    res = True
+    l = 0
+    stack = []
+    if len(s)% 2 != 0:
+        res = False #([])
+        return res
 
-        l = 0
-        r = 1
-
-        res = False 
-        s = "".join(sorted(s))
-        if (len(s) % 2) != 0:
-            res = False
-            return res
+    while l < len(s): #[
+        print(l)
+        if s[l] not in paran_dic.keys():
+            if len(stack) == 0 :
+                res = False
+                return res
+            
+            if paran_dic[stack[-1]] != s[l]:
+                res = False
+                return res
+            else:
+                stack.pop(-1)
+                l = l + 1
+        else:
+            if s[l+1] == paran_dic[s[l]]:
+                l = l+2
+            else:
+                stack.append(s[l]) #[{]
+                l = l+1
+    return res
+        
+print(isValid(s = "{[]}"))
