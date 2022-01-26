edc54ed0e5e14e9f0def2850b0db36b8a5ddc19f
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-01-26 22:57:39 +0600

    Palindromic Substring

--- maxPalindrome.py
+++ maxPalindrome.py
@@ -6,18 +6,35 @@ def longestPalindrome(s):
     if len(s) == 1:
         return s
 
-    while r < len(s):
-        while l <= r: #l = 0  r = 3 
-            if s[l].lower() == s[r].lower(): #ba
-                if s[l : r+1] == s[l : r+1][::-1]:
-                    if r-l+1 > maxLen:
-                        maxPalindrome = s[l : r+1] #bab
-                        maxLen = r-l+1 
-            l += 1
-        l = 0
-        r += 1 # l = 2
     
+    for i in range(len((s))):
+            l = i - 1  # 2
+            r = i + 1 #4 
+            while (l >= 0) and (r < len(s)) : #l = 0  r = 2 
+                if (s[l].lower() == s[r].lower()) : #ba
+                    if (r-l+1 >= maxLen):
+                            maxPalindrome = s[l : r+1] #bab
+                            maxLen = r-l+1 
+                else: 
+                    break
+
+                l -= 1 
+                r += 1
+
+    for i in range(len((s))):   
+            l = i
+            r = i+1
+            while (l >= 0) and (r < len(s)) : #l = 0  r = 2 
+                if (s[l].lower() == s[r].lower()) : #ba
+                    if (r-l+1 >= maxLen):
+                            maxPalindrome = s[l : r+1] #bab
+                            maxLen = r-l+1 
+                else: 
+                    break
+
+                l -= 1 
+                r += 1   
     return maxPalindrome
 
-s = "ac"
+s = "abbc"
 print(longestPalindrome(s))
---
+++ palindromicSubstring.py
@@ -0,0 +1,23 @@
+def countSubstrings(s):
+    count = 0
+    def addPalindrom(s, l, r):
+       
+        while (l>=0 and r <len(s)) and (s[l]==s[r]):
+            nonlocal count
+            count  += 1
+            l -= 1
+            r += 1
+
+    for i in range(len(s)):
+        l= i #0
+        r = i #
+        addPalindrom(s, l, r)
+
+    for i in range(len(s)):
+        l = i
+        r = i + 1     
+        addPalindrom(s, l, r)
+
+    return count     
+
+print(countSubstrings(s = "aaa"))
