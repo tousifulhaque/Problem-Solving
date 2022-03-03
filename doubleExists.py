ebb7ad0e3c0cff813cf505d2fbb2569d53aa56c7
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-03-03 14:53:27 +0600

    Double Exists O(N) solution

--- doubleExists.py
+++ doubleExists.py
@@ -3,22 +3,14 @@ def checkIfExist(arr):
         :type arr: List[int]
         :rtype: bool
         """
-        i = 0
-        j = i + 1
-        isDouble = False
+        exists = set()
+        for i in arr:
+            if i in exists:
+                return True
+            exists.add(i*2)
+            exists.add(i/2)
+        return False
         
-        while i < len(arr):
-            while j < len(arr):
-                if arr[j] == (arr[i] * 2):
-                    isDouble = True 
-                    return isDouble 
-                j += 1
-
-            i += 1
-            j  = i + 1
-        
-        return isDouble
-
 if __name__ == "__main__":
     arr = [1,3,5,7,9]
     print(checkIfExist(arr))
