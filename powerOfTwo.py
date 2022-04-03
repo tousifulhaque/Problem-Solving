7e0fe0c747c1836624019e1819839f1e62794a2d
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-04-03 12:59:28 +0600

    Power of Two WO Loop

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -1,21 +1,34 @@
 # return true if a number is a power of two otherwise return false 
 
-def powerOfTwo(num):
 
-    res = None  #4
+import math
+class Solution(object):
+    def powerOfTwo(self, num):
+        # res = None  #4
 
-    if (num) > 1 and num % 2 == 0:
-        res = powerOfTwo(num/2)
+        # if (num) > 1 and num % 2 == 0:
+        #     res = powerOfTwo(num/2)
 
-    elif ((num) > 1 and num % 2 != 0) or num < 1:
-        return False
-    
-    else: 
-        return True
+        # elif ((num) > 1 and num % 2 != 0) or num < 1:
+        #     return False
+        
+        # else: 
+        #     return True
 
-    return res
+        # return res
+
+        ####### Solution without any loop ########
+
+        if bin(num).count("1") == 1 and num > 0:
+            return True
+        
+        else:
+            return False
+
+
     
 
 if __name__ == '__main__':
-    num = 9
-    print(powerOfTwo(num))
+    num = -29
+    solution = Solution()
+    print(solution.powerOfTwo(num))
