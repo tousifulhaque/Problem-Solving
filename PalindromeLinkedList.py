0bc76004de2d66002883e8b11f0574f040652d1c
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-04-08 09:50:07 +0600

    Palindrome Linked List Recursive Approach

--- PalindromeLinkedList.py
+++ PalindromeLinkedList.py
@@ -25,3 +25,26 @@ def isPalindrome(self, head):
                 itr = itr.next
                 rev_itr = rev_itr.next
         return True
+
+
+    #recursive approach
+
+    # Store the head in as a class property, so that it can be accessed and changed it the last recursion step
+    self.frontpointer = head
+
+    def recursive_approach(current_node = head):
+       
+        # Recurse to the end of the Linked List and return a False so that the next lines can be executed
+        if not (recursive_approach(current_node.next)) :
+            return False
+        
+        # Check if the current_node val is equal to the frontpointer value if they are move the frontpointer forward.
+        # If they aren't equal return False to the upper stack which will perpetuate to the upper function calls
+        if current_node.val  != self.frontpointer.val :
+            return False
+
+        self.frontpointer = self.frontpointer.next
+
+        return True
+    
+    return recursive_approach()
